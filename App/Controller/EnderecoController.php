4f9ca1e706e82a96f01f1a14450b240c5913d894
gaboliveiradev <g38789138@gmail.com>
2023-02-13 09:36:00 -0300

    realizando teste com o metodo getResponseAsJSON

--- App/Controller/EnderecoController.php
+++ App/Controller/EnderecoController.php
@@ -2,5 +2,9 @@
 namespace App\Controller;
 
 class EnderecoController extends Controller {
-    
+
+    public static function teste() {
+        //var_dump("Rafael Sanchez... Moçada... To Inu... Tiu");
+        parent::getResponseAsJSON($arr_cidades = ["Bariri", "Jau", "Itapuí", "Bauru"]);
+    }
 }
--- App/rotas.php
+++ App/rotas.php
@@ -1,13 +1,15 @@
 <?php
-/*use App\Controller\{};*/
+use App\Controller\{
+    EnderecoController
+};
 
 $parse_uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 switch($parse_uri) {
 
     
-    case "/welcome":
-        
+    case "/teste":
+        EnderecoController::teste();
     break;
 
     default:
