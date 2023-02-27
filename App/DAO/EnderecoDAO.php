cb917a352de17ac0517315a451720bdfd68d76a8
gaboliveiradev <g38789138@gmail.com>
2023-02-27 09:36:58 -0300

    finalizando o metodo selectByCep e selectCidadesByUf da dao

--- App/DAO/EnderecoDAO.php
+++ App/DAO/EnderecoDAO.php
@@ -11,7 +11,25 @@ class EnderecoDAO extends DAO {
     
     function selectByCep(int $cep) 
     {
+        $sql = "SELECT * FROM logradouro WHERE cep = ?";
 
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->bindValue(1, $cep);
+        $stmt->execute();
+
+        $endereco_obj = $stmt->fetchObject("App\Model\EnderecoModel");
+        $endereco_obj = $stmt->selectCidadesByUf($endereco_obj->UF);
+
+        return $endereco_obj;
+    }
+
+    function selectCidadesByUf($uf) 
+    {
+        $sql = "SELECT * FROM cidade WHERE uf = ? ORDER BY descricao";
+
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->bindValue(1, $uf);
+        $stmt->execute();
     }
 
     function selectCepByLogradouro(string $logradouro) 
