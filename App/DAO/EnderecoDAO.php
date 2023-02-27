86ef2d19d37b84bbbbc8633b73ffa3c005017496
gaboliveiradev <g38789138@gmail.com>
2023-02-27 08:55:07 -0300

    finalizando o metodo getLogradourouByCep na model e criando ele na dao

--- App/DAO/EnderecoDAO.php
+++ App/DAO/EnderecoDAO.php
@@ -3,4 +3,8 @@ namespace App\DAO;
 
 class EnderecoDAO extends DAO {
     
+    function selectByCep(int $cep) 
+    {
+
+    }
 }
--- App/Model/EnderecoModel.php
+++ App/Model/EnderecoModel.php
@@ -1,10 +1,24 @@
 <?php
 namespace App\Model;
 
+use App\DAO\EnderecoDAO;
+use Exception;
+
 class EnderecoModel extends Model {
     
     public $id_logradouro, $tipo, $descricao, $id_cidade, $uf, $complemento, 
     $descricao_sem_numero, $descricao_cidade, $codigo_cidade_ibge, $descricao_bairro;
 
     public $arr_cidades;
+
+    public function getLogradouroByCep(int $cep)
+    {
+        try 
+        {
+            $dao = new EnderecoDAO();
+            return $dao->selectByCep($cep);
+        } catch(Exception $err) {
+            throw $err;
+        }
+    }
 }
