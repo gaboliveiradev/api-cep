be9ce4c9db8c002b2d81031f2ab53bdf17d60299
gaboliveiradev <g38789138@gmail.com>
2023-02-27 08:29:29 -0300

    criando as rotas da nossa api

--- App/Controller/EnderecoController.php
+++ App/Controller/EnderecoController.php
@@ -7,4 +7,20 @@ class EnderecoController extends Controller {
         //var_dump("Rafael Sanchez... Moçada... To Inu... Tiu");
         parent::getResponseAsJSON($arr_cidades = ["Bariri", "Jau", "Itapuí", "Bauru"]);
     }
+
+    public static function getLogradouroByCep() {
+
+    }
+
+    public static function getLogradouroByBairroAndCidade() {
+        
+    }
+
+    public static function getCidadesByUf() {
+
+    }
+
+    public static function getBairrosByCidade() {
+        
+    }
 }
--- App/rotas.php
+++ App/rotas.php
@@ -7,12 +7,28 @@ $parse_uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 switch($parse_uri) {
 
-    
-    case "/teste":
-        EnderecoController::teste();
+    // [OK] https://localhost:8000/endereco/by-cep?cep=
+    case "/endereco/by-cep":
+        EnderecoController::getLogradouroByCep();
     break;
 
+    // [OK] https://localhost:8000/logradouro/by-bairro?bairro=
+    case "/logradouro/by-bairro":
+        EnderecoController::getLogradouroByBairroAndCidade();
+    break;
+
+    // [OK] https://localhost:8000/cidades/by-uf?uf=
+    case "/cidades/by-uf":
+        EnderecoController::getCidadesByUf();
+    break;
+
+    // [OK] https://localhost:8000/bairro/by-cidade?cidade=
+    case "/bairro/by-cidade":
+        EnderecoController::getBairrosByCidade();
+    break;
+
+    //[403] - Servidor recebeu a requisição, indentificou o autor. Porém não autorizou a emissão da resposta.
     default:
-        header("Location: /welcome");
+        http_response_code(403);
     break;
 }
