f24062102e42480d4f8afecab4f69ba3055b582e
gaboliveiradev <g38789138@gmail.com>
2023-02-27 08:47:15 -0300

    criando as propriedades da minha classe EnderecoModel

--- App/Model/EnderecoModel.php
+++ App/Model/EnderecoModel.php
@@ -3,4 +3,8 @@ namespace App\Model;
 
 class EnderecoModel extends Model {
     
+    public $id_logradouro, $tipo, $descricao, $id_cidade, $uf, $complemento, 
+    $descricao_sem_numero, $descricao_cidade, $codigo_cidade_ibge, $descricao_bairro;
+
+    public $arr_cidades;
 }
