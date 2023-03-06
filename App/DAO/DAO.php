06527858d3036a38b13142e0f2ad0b7b02becd7c
gaboliveiradev <g38789138@gmail.com>
2023-03-06 08:37:51 -0300

    reajustando a classe abstrata dao e a config.php

--- App/DAO/DAO.php
+++ App/DAO/DAO.php
@@ -1,13 +1,25 @@
 <?php
 
 namespace App\DAO;
-use \PDO;
 
-abstract class DAO {
+use \PDO;
+use \Exception;
+use \PDOException;
+
+abstract class DAO extends PDO {
 
     protected $conexao;
     public function __construct() {
-        $dsn = "mysql:host=" . $_ENV['db']['host'] . ";dbname=" . $_ENV['db']['dbname'];
-        $this->conexao = new PDO($dsn, $_ENV['db']['user'], $_ENV['db']['pass']);
+        try {
+            $options = [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
+            ];
+
+            $dsn = "mysql:host=" . $_ENV['db']['host'] . ";dbname=" . $_ENV['db']['dbname'];
+            $this->conexao = new PDO($dsn, $_ENV['db']['user'], $_ENV['db']['pass'], $options);
+        } catch (PDOException $err) {
+            throw new Exception("Ocorreu um erro ao tentar conectar-se ao MySQL", 0, $err);
+        }
     }
 }
